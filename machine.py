7e9613cbd2ee09c39aa27608cdc3144629d88720
Ruturaj Mohite <53974118+gr455@users.noreply.github.com>
2020-08-24 13:26:59 +0530

    Fix multiple bugs and binary to decimal
    why binary why

--- machine.py
+++ machine.py
@@ -1,27 +1,30 @@
+def decToBin(n):  
+    return bin(n).replace("0b", "")
 
 class Machine:
 
-	def __init__(self, memsize):
+	def __init__(self, memsize, instrStart):
 
-		self.memory = Memory(memsize, memwordsize)
-		self.mar = MAR()
-		self.mbr = MBR()
-		self.ibr = IBR()
-		self.ir =  IR()
-		self.controlCircuits = ControlCircuits()
-		self.alc = ALC()
-		self.accumulator = Accumulator()
-		self.mq = MQ()
-		self.pc = PC()
+		self.memory = Memory(self, memsize)
+		self.mar = MAR(self)
+		self.mbr = MBR(self)
+		self.ibr = IBR(self)
+		self.ir =  IR(self)
+		self.controlCircuits = ControlCircuits(self)
+		self.alc = ALC(self)
+		self.accumulator = Accumulator(self)
+		self.mq = MQ(self)
+		self.pc = PC(self, instrStart)
 
 	# start the machine
-	def start():
+	def start(self):
+		input(f"Machine start at instruction pointer at location {self.pc.counter} in memory\nEnter to start")
 		while self.pc.counter < self.memory.size:
 			try:
 			# start instruction cycle
-				self.mar.load(self.memory.memory[self.counter], startWord)
+				self.mar.load(Instruction.padAddress(decToBin(self.pc.counter)))
+				self.mar.fetch(self.pc.startWord)
 				self.pc.startWord = 0
-				self.mar.fetch()
 				self.mbr.unload()
 				while self.ir.buffer:
 					self.ir.unload()
@@ -29,7 +32,7 @@ class Machine:
 					# end of instruction cycle
 
 					# start data cycle
-					func(self.mbr.buffer)
+					func()
 					self.mbr.buffer = None
 					# end data cycle
 
@@ -38,9 +41,11 @@ class Machine:
 
 				self.pc.counter += 1
 			except Exception as err:
-				print("Exception at pc = {self.pc.counter}")
+				print(f"Exception at pc = {self.pc.counter}")
 				raise err
-
+		print(f"Program counter encountered memory boundary or end of input at pc = {self.pc.counter}")
+		print("Memory state at the end of execution:")
+		self.memory.prettyPrint()
 		return 0
 
 
@@ -49,16 +54,16 @@ class Instruction:
 	# for a single instruction
 	@staticmethod
 	def opcode(instruction):
-		return instruction[0:7]
+		return instruction[0:8]
 
 	@staticmethod
 	def data(instruction):
-		return instruction[8:19]
+		return instruction[8:20]
 
 	# for a word
 	@staticmethod
 	def seperate(instructions):
-		return (instructions[0:19], instructions[20:39])
+		return (instructions[0:20], instructions[20:40])
 
 	@staticmethod
 	def leftRequired(instructions):
@@ -78,59 +83,104 @@ class Instruction:
 
 		return instruction
 
+	@staticmethod
+	def padWord(word):
+		while(len(word) < 40):
+			word = '0' + word
+
+		return word
+
 class Memory(Machine):
 
-	def __init__(self, size):
+	def __init__(self, machine, size):
+		self.m = machine
 		self.memory = ['0' * 40] * size # 8 bit opcode + 12 bit address * 2
 		self.size = size
 
-		self.instruction_set = {
+	def printMemory(self):
+		for i in self.memory:
+			print(i)
+
+	def prettyPrint(self):
+		zcount = 0
+		flag = 0
+		for i in range(len(self.memory)+1):
+			try:
+				if self.memory[i] == "0"*40 and i <= self.size - 1:
+					if not zcount:
+						print(f"{i} >> ".rjust(8) + "0"*40, end=" ")
+					zcount+=1
+					flag = 1
+				else:
+					if flag:
+						print(f"x {zcount}")
+						flag = 0
+					zcount = 0
+					if i <= self.size - 1:
+						print(f"{i} >> ".rjust(8) + f"{self.memory[i]}")
+			except IndexError:
+				print(f"x {zcount}")
+				zcount = 0
+				if i <= self.size - 1:
+					print(f"{i} >> {self.memory[i]}")
+
+	def instrMap(self, opcode):
+		instruction_set = {
 			# data transfer
-			'00001010': self.controlCircuits.loadMq,
-			'00001001': self.controlCircuits.loadMqMx,
-			'00100001': self.controlCircuits.storMx,
-			'00000001': self.controlCircuits.loadMx,
-			'00000010': self.controlCircuits.loadMinusMx,
-			'00000011': self.controlCircuits.loadAbsMx,
-			'00000100': self.controlCircuits.loadMinusAbsMx,
+			'00001010': self.m.controlCircuits.loadMq,
+			'00001001': self.m.controlCircuits.loadMqMx,
+			'00100001': self.m.controlCircuits.storMx,
+			'00000001': self.m.controlCircuits.loadMx,
+			'00000010': self.m.controlCircuits.loadMinusMx,
+			'00000011': self.m.controlCircuits.loadAbsMx,
+			'00000100': self.m.controlCircuits.loadMinusAbsMx,
 			# unconditional
-			'00001101': self.controlCircuits.jumpMxL,
-			'00001110': self.controlCircuits.jumpMxR,
+			'00001101': self.m.controlCircuits.jumpMxL,
+			'00001110': self.m.controlCircuits.jumpMxR,
 			# conditional
-			'00001111': self.controlCircuits.jumpPlusMxL,
-			'00010000': self.controlCircuits.jumpPlusMxR,
+			'00001111': self.m.controlCircuits.jumpPlusMxL,
+			'00010000': self.m.controlCircuits.jumpPlusMxR,
 			# arithmetic
-			'00000101': self.controlCircuits.addMx,
-			'00000111': self.controlCircuits.addAbsMx,
-			'00000110': self.controlCircuits.subMx,
-			'00001000': self.controlCircuits.subAbsMx,
-			'00001011': self.controlCircuits.mulMx,
-			'00001100': self.controlCircuits.divMx,
-			'00010100': self.controlCircuits.lsh,
-			'00010101': self.controlCircuits.rsh,
+			'00000101': self.m.controlCircuits.addMx,
+			'00000111': self.m.controlCircuits.addAbsMx,
+			'00000110': self.m.controlCircuits.subMx,
+			'00001000': self.m.controlCircuits.subAbsMx,
+			'00001011': self.m.controlCircuits.mulMx,
+			'00001100': self.m.controlCircuits.divMx,
+			'00010100': self.m.controlCircuits.lsh,
+			'00010101': self.m.controlCircuits.rsh,
 			# address modify
-			'00010010': self.controlCircuits.storMxL,
-			'00010011': self.controlCircuits.storMxR,
+			'00010010': self.m.controlCircuits.storMxL,
+			'00010011': self.m.controlCircuits.storMxR,
 			# no op
-			'10000000': self.controlCircuits.noOp
+			'10000000': self.m.controlCircuits.noOp,
+			'00000000': self.m.controlCircuits.noOp
 		}
+		return instruction_set[opcode]
 
 	def store(self, data, address):
-		self.memory[address] = data
+		if type(address) == 'str':
+			address = int(str,2)
+		if type(data) != "str":
+			data = decToBin(data)
+		self.memory[address] = Instruction.padWord(data)
 
 class MAR(Machine):
 
-	def __init__(self):
+	def __init__(self, machine):
 		self.buffer = None
+		self.m = machine
 
 	def load(self, address):
 		self.buffer = address
 
 	def fetch(self, startWord = 0):
+		# print(f"fetch called on mar, buffer = {self.buffer} ")
 		if self.buffer:
 			try:
-				self.mbr.load(self.memory.memory[self.buffer][startWord:])
-			except IndexError:
+				# print(int(self.buffer,2))
+				self.m.mbr.load(self.m.memory.memory[int(self.buffer,2)][startWord:])
+			except IndexError as e:
 				raise MemoryAccessError("Attempted to access invalid memory location")
 		else:
 			raise UnloadNoneError("Attempted to unload data from empty buffer")
@@ -140,24 +190,27 @@ class MAR(Machine):
 
 class MBR(Machine):
 
-	def __init__(self):
+	def __init__(self, machine):
 		self.buffer = None
+		self.m = machine
 
 	def load(self, instructions):
 		self.buffer = instructions
 
 	# moves the instructions from mbr to ir/ibr 
 	def unload(self):
+		# print(f"ul called on mbr, buffer = {self.buffer}")
 		instructions = self.buffer
 
 		if instructions:
 			if Instruction.leftRequired(instructions):
-				self.ir.load(instructions[0:7])
-				self.ibr.load(instructions[20:39])
-				self.mar.load(instructions[8:19])
+				self.m.ir.load(instructions[0:8])
+				self.m.ibr.load(instructions[20:40])
+				self.m.mar.load(instructions[8:20])
 			else:
-				self.ir.load(instructions[20:27])
-				self.mar.load(instructions[28:39])
+				self.m.ir.load(instructions[20:28])
+				self.m.mar.load(instructions[28:40])
+				# print("pr >>" + instructions[8:20]+ str(self.m.pc.counter))
 
 			self.buffer = None
 
@@ -165,15 +218,15 @@ class MBR(Machine):
 			raise UnloadNoneError("Attempted to unload data from empty buffer")
 
 	def storeToMemory(self, address):
-		self.memory.store(self.buffer, address)
+		self.m.memory.store(self.buffer, address)
 		self.buffer = None
 
 	def changeAddress(self, address, leftOrRight = 'left'):
 		if leftOrRight == 'left':
-			self.memory.memory[self.buffer] = self.memory.memory[self.buffer][0:8] + address + self.memory.memory[self.buffer][20:39]
+			self.memory.memory[self.buffer] = self.memory.memory[self.buffer][0:8] + address + self.memory.memory[self.buffer][20:40]
 
-		else if leftOrRight == 'right':
-			self.memory.memory[self.buffer] = self.memory.memory[self.buffer][0:19] +  self.memory.memory[self.buffer][20:27] + address
+		elif leftOrRight == 'right':
+			self.memory.memory[self.buffer] = self.memory.memory[self.buffer][0:20] +  self.memory.memory[self.buffer][20:28] + address
 
 		else:
 			raise IllegalOperationError("An internal function was passed illegal paramerter(s)")
@@ -182,8 +235,9 @@ class MBR(Machine):
 
 class IBR(Machine):
 
-	def __init__(self):
+	def __init__(self, machine):
 		self.buffer = None
+		self.m = machine
 
 	def load(self, instruction):
 		self.buffer = instruction
@@ -192,8 +246,8 @@ class IBR(Machine):
 		instruction = self.buffer
 
 		if instruction:
-			self.ir.load(instruction[0:7])
-			self.mar.load(instruction[8:19])
+			self.m.ir.load(instruction[0:8])
+			self.m.mar.load(instruction[8:20])
 		else:
 			raise UnloadNoneError("Attempted to unload data from empty buffer")
 
@@ -204,15 +258,17 @@ class IBR(Machine):
 
 class IR(Machine):
 
-	def __init__(self):
+	def __init__(self, machine):
 		self.buffer = None
+		self.m = machine
 
 	def load(self, opcode):
 		self.buffer = opcode
 
 	def unload(self):
+		# print(f"ul called on IR, buffer = {self.buffer}")
 		if(self.buffer):
-			self.controlCircuits.load(self.buffer)
+			self.m.controlCircuits.load(self.buffer)
 		else:
 			raise UnloadNoneError("Attempted to unload data from empty buffer")
 
@@ -220,156 +276,165 @@ class IR(Machine):
 
 class ControlCircuits(Machine):
 
-	def __init__(self):
+	def __init__(self, machine):
 		self.buffer = None
+		self.m = machine
 
 	def load(self, opcode):
 		self.buffer = opcode
 
 	def decodeInstruction(self):
-		return self.memory.instruction_set[self.buffer]
+		return self.m.memory.instrMap(self.buffer)
 
 	# operation functions definition
 
 	def loadMq(self):
-		self.mq.moveToAcc()
+		self.m.mq.moveToAcc()
 
 	# Mx should be data
 	def loadMqMx(self):
-		self.mar.fetch() # since loadMqMx first is loaded to MBR as an instruction, MBR now expects data
+		self.m.mar.fetch() # since loadMqMx first is loaded to MBR as an instruction, MBR now expects data
 
-		self.alc.moveToMQ()
+		self.m.alc.moveToMQ()
 
 	def storMx(self):
-		self.alc.moveToMbr()
-		self.mbr.storeToMemory(self.mbr.buffer)
+		self.m.alc.moveToMbr()
+		self.m.mbr.storeToMemory(int(self.m.mar.buffer,2))
 
 	def loadMx(self):
-		self.mar.fetch()
+		self.m.mar.fetch()
 
-		self.alc.moveToAcc()
+		self.m.alc.moveToAcc()
 
 	def loadMinusMx(self):
-		self.mar.fetch()
-		self.mbr.buffer = -self.mbr.buffer
+		self.m.mar.fetch()
+		self.m.mbr.buffer = -self.m.mbr.buffer
 
-		self.alc.moveToAcc()
+		self.m.alc.moveToAcc()
 
 	def loadAbsMx(self):
-		self.mar.fetch()
-		self.mbr.buffer = abs(self.mbr.buffer)
+		self.m.mar.fetch()
+		self.m.mbr.buffer = abs(self.m.mbr.buffer)
 
-		self.alc.moveToAcc()
+		self.m.alc.moveToAcc()
 
 	def loadMinusAbsMx(self):
 		self.mar.fetch()
-		self.mbr.buffer = -abs(self.mbr.buffer)
+		self.mbr.buffer = -abs(self.m.mbr.buffer)
 
 		self.alc.moveToAcc()
 
 	def jumpMxL(self):
-		self.mar.fetch()
-		self.pc.counter = int(self.mbr.buffer,2)
-		self.pc.startWord = 0
+		self.m.mar.fetch()
+		self.m.pc.counter = int(self.m.mbr.buffer,2)
+		self.m.pc.startWord = 0
 
 	def jumpMxR(self):
-		self.mar.fetch()
-		self.pc.counter = int(self.mbr.buffer,2)
-		self.pc.startWord = 20
+		self.m.mar.fetch()
+		self.m.pc.counter = int(self.m.mbr.buffer,2)
+		self.m.pc.startWord = 20
 
 	def jumpPlusMxL(self):
-		self.mar.fetch()
-		if self.accumulator.buffer >= 0 :
-			self.pc.counter = int(self.mbr.buffer,2)
-			self.pc.startWord = 0
+		self.m.mar.fetch()
+		if self.m.accumulator.buffer >= 0 :
+			self.m.pc.counter = int(self.m.mbr.buffer,2)
+			self.m.pc.startWord = 0
 
 	def jumpPlusMxR(self):
-		self.mar.fetch()
-		if self.accumulator.buffer >= 0 :
-			self.pc.counter = int(self.mbr.buffer,2)
-			self.pc.startWord = 20
+		self.m.mar.fetch()
+		if self.m.accumulator.buffer >= 0 :
+			self.m.pc.counter = int(self.m.mbr.buffer,2)
+			self.m.pc.startWord = 20
 
 	def addMx(self):
-		self.mar.fetch()
-		self.alc.add(self.mbr.buffer, self.accumulator.buffer)
-		self.mbr.buffer = None
+		self.m.mar.fetch()
+		self.m.alc.add(int(self.m.mbr.buffer,2), int(self.m.accumulator.buffer,2))
+		self.m.mbr.buffer = None
 
 	def addAbsMx(self):
-		self.mar.fetch()
-		self.alc.add(abs(self.mbr.buffer), self.accumulator.buffer)
-		self.mbr.buffer = None
+		self.m.mar.fetch()
+		self.m.alc.add(abs(int(self.m.mbr.buffer,2)), int(self.m.accumulator.buffer,2))
+		self.m.mbr.buffer = None
 
 	def subMx(self):
-		self.mar.fetch()
-		self.alc.subtract(self.accumulator.buffer, self.mbr.buffer)
-		self.mbr.buffer = None
+		self.m.mar.fetch()
+		self.m.alc.subtract(int(self.m.accumulator.buffer,2), int(self.m.mbr.buffer,2))
+		self.m.mbr.buffer = None
 
 	def subAbsMx(self):
-		self.mar.fetch()
-		self.alc.subtract(self.accumulator.buffer, abs(self.mbr.buffer))
-		self.mbr.buffer = None
+		self.m.mar.fetch()
+		self.m.alc.subtract(int(self.m.accumulator.buffer,2), abs(int(self.m.mbr.buffer,2)))
+		self.m.mbr.buffer = None
 
 	def mulMx(self):
-		self.mar.fetch()
-		self.alc.multiply(self.mq.buffer, self.mbr.buffer)
-		self.mbr.buffer = None
+		self.m.mar.fetch()
+		self.m.alc.multiply(self.m.mq.buffer, self.m.mbr.buffer)
+		self.m.mbr.buffer = None
 
 	def divMx(self):
-		self.mar.fetch()
-		self.alc.divide(self.accumulator.buffer, self.mbr.buffer)
-		self.mbr.buffer = None
+		self.m.mar.fetch()
+		self.m.alc.divide(self.m.accumulator.buffer, self.m.mbr.buffer)
+		self.m.mbr.buffer = None
 
 	def lsh(self):
-		self.mar.fetch()
-		self.alc.leftShift(self.mbr.buffer)
-		self.mbr.buffer = None
+		self.m.mar.fetch()
+		self.m.alc.leftShift(self.m.mbr.buffer)
+		self.m.mbr.buffer = None
 
 	def rsh(self):
-		self.mar.fetch()
-		self.alc.rightShift(self.mbr.buffer)
-		self.mbr.buffer = None
+		self.m.mar.fetch()
+		self.m.alc.rightShift(self.m.mbr.buffer)
+		self.m.mbr.buffer = None
 
 	def noOp(self):
 		return True
 
 	def storMxL(self):
-		acBits = self.accumulator.buffer & 2**12 - 1
+		acBits = self.m.accumulator.buffer & 2**12 - 1
 		acBits = Instruction.padAddress(str(acBits))
-		self.mbr.load(acBits)
+		self.m.mbr.load(acBits)
 		self.changeAddress(acBits, 'left')
 
-	def storMxR(self);
-		acBits = self.accumulator.buffer & 2**12 - 1
+	def storMxR(self):
+		acBits = self.m.accumulator.buffer & 2**12 - 1
 		acBits = Instruction.padAddress(str(acBits))
-		self.mbr.load(acBits)
+		self.m.mbr.load(acBits)
 		self.changeAddress(acBits, 'right')
 
 
 class ALC(Machine):
+
+	def __init__(self, machine):
+		self.m = machine
 	
 	# moves value from MBR to MQ
 	def moveToMQ(self):
-		self.mq.load(self.mbr.buffer)
-		self.mbr.buffer = None
+		self.m.mq.load(self.m.mbr.buffer)
+		self.m.mbr.buffer = None
 
 	# moves value from accumulator to MBR
 	def moveToMbr(self):
-		self.mbr.load(self.accumulator.buffer)
-		self.accumulator.buffer = None
+		self.m.mbr.load(self.m.accumulator.buffer)
+		self.m.accumulator.buffer = None
 
 	# moves value from MBR to accumulator 
 	def moveToAcc(self):
-		self.accumulator.load(self.mbr.buffer)
-		self.mbr.buffer = None
+		self.m.accumulator.load(self.m.mbr.buffer)
+		self.m.mbr.buffer = None
 
 
 	def add(self, a, b):
-		self.accumulator.load(a + b)
+		self.m.accumulator.load(a + b)
 
 	def subtract(self, a , b):
-		self.accumulator.load(a - b)
+		self.m.accumulator.load(a - b)
 
 	def multiply(self, a, b):
+		# print(a, b)
+		if type(a).__name__ == "str":
+			a = int(a,2)
+		if type(b).__name__ == "str":
+			b = int(b,2)
 		mul = a * b
 		isNegative = mul < 0
 		mul = abs(mul)
@@ -382,35 +447,49 @@ class ALC(Machine):
 			toMq = mul & 2**(39 - bitLength) - 1 # least significant excess bits
 			toAcc = mul >> (39 - bitLength)
 
-		self.accumulator.load(toAcc)
-		self.mq.load(toMq, isNegative)
+		self.m.accumulator.load(toAcc)
+		self.m.mq.load(toMq, isNegative)
 
 	def divide(self, a, b):
+
+		if type(a).__name__ == "str":
+			a = int(a,2)
+		if type(b).__name__ == "str":
+			b = int(b,2)
+
 		quotient = a // b
 		remainder = a % b
 
-		self.accumulator.load(remainder)
-		self.mq.load(quotient)
+		self.m.accumulator.load(remainder)
+		self.m.mq.load(quotient)
 
 	def leftShift(self, a):
-		self.accumulator.load(a << 1)
+		if type(a).__name__ == "str":
+			a = int(a,2)
+
+		self.m.accumulator.load(a << 1)
 
 	def rightShift(self, a):
-		self.accumulator.load(a >> 1)
+		if type(a).__name__ == "str":
+			a = int(a,2)
+			
+		self.m.accumulator.load(a >> 1)
 
 
 class Accumulator(Machine):
 
-	def __init__(self):
+	def __init__(self, machine):
 		self.buffer = None
+		self.m = machine
 
 	def load(self, data):
 		self.buffer = data
 
 class MQ(Machine):
-	def __init__(self):
+	def __init__(self, machine):
 		self.buffer = None
 		self.isNegative = False
+		self.m = Machine
 
 	def load(self, data, isNegative = False):
 		self.buffer = data
@@ -421,7 +500,8 @@ class MQ(Machine):
 		self.buffer = None
 
 class PC(Machine):
-	def __init__(self, start):
+	def __init__(self, machine, start):
+		self.m = machine
 		self.counter = int(start,2)
 		self.startWord = 0
 
@@ -435,3 +515,20 @@ class MemoryAccessError(IndexError):
 
 class IllegalOperationError(Exception):
 	''' to be raised when a function is passed invalid argument '''
+
+
+def main():
+	'''
+	Add instructions and data to the memory and initialize program counter start position
+	'''
+	machine = Machine(1000,'00100000')
+	machine.memory.memory[0] = "0000000000000000000000000000000000000101"
+	machine.memory.memory[1] = "0000000000000000000000000000000000000011"
+	machine.memory.memory[32] = '0000100100000000000000001011000000000001'
+									 # "100000000000000000101000000000001"
+	machine.memory.memory[33] = '0000000000000000000000100001000000000010'
+	machine.start()
+
+	pass
+
+main()
